a9ef76f7b11fa79f855fe88659cd7efdee9fd773
Suzy <susan.m.mullin@gmail.com>
2018-11-04 10:36:31 -0800

    added user sign in and out

--- spec/integration/users_spec.js
+++ spec/integration/users_spec.js
@@ -2,8 +2,8 @@ const request = require('request');
 const server = require('../../src/server');
 const base = 'http://localhost:3000/users/';
 const User = require('../../src/db/models').User;
-
 const sequelize = require('../../src/db/models/index').sequelize;
+
 describe('routes : users', () => {
   beforeEach(done => {
     sequelize
@@ -16,6 +16,7 @@ describe('routes : users', () => {
         done();
       });
   });
+
   describe('GET /users/sign_up', () => {
     it('should render a view with a sign up form', done => {
       request.get(`${base}sign_up`, (err, res, body) => {
@@ -34,6 +35,7 @@ describe('routes : users', () => {
           password: '123456789'
         }
       };
+
       request.post(options, (err, res, body) => {
         User.findOne({ where: { email: 'user@example.com' } })
           .then(user => {
@@ -48,6 +50,7 @@ describe('routes : users', () => {
           });
       });
     });
+
     it('should not create a new user with invalid attributes and redirect', done => {
       request.post(
         {
@@ -69,6 +72,15 @@ describe('routes : users', () => {
             });
         }
       );
+    });
+  });
+  describe('GET /users/sign_in', () => {
+    it('should render a view with a sign in form', done => {
+      request.get(`${base}sign_in`, (err, res, body) => {
+        expect(err).toBeNull();
+        expect(body).toContain('Sign in');
+        done();
+      });
     });
   });
 });
--- src/routes/validation.js
+++ src/routes/validation.js
@@ -1,7 +1,7 @@
 const User = require('../db/models').User;
 
 module.exports = {
-  validateUsers(req, res, next) {
+  validateSignup(req, res, next) {
     if (req.method === 'POST') {
       req
         .checkBody('username', 'must be at least 4 characters in length')
@@ -14,6 +14,21 @@ module.exports = {
         .checkBody('passwordConfirmation', 'must match password provided')
         .optional()
         .matches(req.body.password);
+    }
+    const errors = req.validationErrors();
+    if (errors) {
+      req.flash('error', errors);
+      return res.redirect(req.headers.referer);
+    } else {
+      return next();
+    }
+  },
+  validateSignin(req, res, next) {
+    if (req.method === 'POST') {
+      req.checkBody('email', 'must be valid').isEmail();
+      req
+        .checkBody('password', 'must be at least 6 characters in length')
+        .isLength({ min: 6 });
     }
     const errors = req.validationErrors();
     if (errors) {
