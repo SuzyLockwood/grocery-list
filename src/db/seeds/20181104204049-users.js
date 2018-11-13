84cfadabc3882eacf46db22a064d84095c8e3939
Suzy <susan.m.mullin@gmail.com>
2018-11-12 23:11:35 -0800

    minor change on seeding for master

--- src/db/seeds/20181104204049-users.js
+++ src/db/seeds/20181104204049-users.js
@@ -5,7 +5,6 @@ let users = [];
 
 for (let i = 1; i <= 5; i++) {
   users.push({
-    id: i,
     username: faker.internet.userName(),
     email: faker.internet.email(),
     password: faker.internet.password(),
